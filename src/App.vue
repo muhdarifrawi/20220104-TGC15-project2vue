943ad07af54955e3b47a6f1bafc053fca1d3d906
arif <muhdarifrawi@gmail.com>
2022-01-08 19:48:14 +0800

    added 'v-bind class:active to nav links.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,54 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a
-            v-on:click="goProductReviews"
-            class="nav-link active"
-            aria-current="page"
-          >
-            Product Reviews
-          </a>
-        </li>
-        <li class="nav-item">
-          <a v-on:click="goProductEntry" class="nav-link">
-            Product Entry
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">About Us</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a
+              v-on:click="goProductReviews"
+              class="nav-link"
+              v-bind:class="{ active: page === 'product-review' }"
+              aria-current="page"
+            >
+              Product Reviews
+            </a>
+          </li>
+          <li class="nav-item">
+            <a
+              v-on:click="goProductEntry"
+              class="nav-link"
+              v-bind:class="{ active: page === 'product-entry' }"
+            >
+              Product Entry
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">About Us</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+
+  <div>
+    <Main v-if="page === 'product-review'" />
+    <AddListing v-if="page === 'product-entry'" />
   </div>
-</nav>
-  
-<div>
-  <Main v-if="page==='product-review'"/>
-  <AddListing v-if="page==='product-entry'" />
-</div>
-  
 </template>
 
 <script>
@@ -49,17 +61,17 @@ export default {
     Main,
     AddListing,
   },
-  data:function(){
-    return{
-      "page":"product-review"
-    }
+  data: function () {
+    return {
+      page: "product-entry",
+    };
   },
   methods: {
     goProductReviews: function () {
-      this.page="product-review"
+      this.page = "product-review";
     },
     goProductEntry: function () {
-      this.page="product-entry"
+      this.page = "product-entry";
     },
   },
 };
@@ -72,7 +84,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-a:hover{
+a:hover {
   cursor: pointer;
 }
 </style>
