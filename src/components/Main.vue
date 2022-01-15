a1fe86cb99b43f41759563b9f99a540383e71d5a
arif <muhdarifrawi@gmail.com>
2022-01-15 13:03:55 +0800

    added card format into iteration.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,30 +1,40 @@
 <template>
-    <div v-for="items in database" :key="items.itemName">
-        {{items.itemName}}
+  <div v-for="items in database" :key="items.itemName">
+    {{ items.itemName }}
+  </div>
+
+  <div class="card m-3" style="width: 18rem" v-for="items in database" :key="items.itemName">
+    <div class="card-body">
+      <h5 class="card-title">{{items.itemName}}</h5>
+      <h6 class="card-subtitle mb-2 text-muted"> Sumitted on {{items.date}} under {{items.category}} submitted by {{items.user}}</h6>
+      <p class="card-text">
+          {{items.itemDescription}}
+      </p>
+      
     </div>
-    <!-- <p>{{database}}</p> -->
+  </div>
+  <!-- <p>{{database}}</p> -->
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 // need to enable CORS on the express side
 // npm install CORS? https://expressjs.com/en/resources/middleware/cors.html
 console.log(process.env.VUE_APP_DEV_MONGO_URL);
 export default {
-    name:"Main",
-    created: async function(){
-        let response = await axios.get(process.env.VUE_APP_DEV_MONGO_URL);
-        this.database = response.data;
-    },
-    data:function() {
-     return{
-        database:"this is a test"
-     }   
-    }
-}
+  name: "Main",
+  created: async function () {
+    let response = await axios.get(process.env.VUE_APP_DEV_MONGO_URL);
+    this.database = response.data;
+  },
+  data: function () {
+    return {
+      database: "this is a test",
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
